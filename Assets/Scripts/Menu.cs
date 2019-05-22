5e77184a85a7c81027707802e7c06db23e5bd33e
Max Bronstring <max.brons@hotmail.com>
2019-05-22 14:51:15 +0200

    UI

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void launch()
+    {
+
+    }
+
+    public void continueToPlay()
+    {
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
