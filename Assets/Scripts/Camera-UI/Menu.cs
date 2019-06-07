1e02ea78a3bfc50e76aac1f1b85d5e09c2366557
Max Bronstring <max.brons@hotmail.com>
2019-06-07 09:27:39 +0200

    Animal animations/ UI

--- Assets/Scripts/Camera-UI/Menu.cs
+++ Assets/Scripts/Camera-UI/Menu.cs
@@ -7,11 +7,8 @@ public class Menu : MonoBehaviour
     public Text timerTime;
     private float timer = 10f;
     public bool setTimer = false;
-<<<<<<< HEAD:Assets/Scripts/Camera-UI/Menu.cs
+
     public GameObject FadeOut;
-=======
-    
->>>>>>> 47d8df74c7e61290069826bd3e1cbcc207dc267c:Assets/Scripts/Menu.cs
 
     public void PlayGame()
     {
